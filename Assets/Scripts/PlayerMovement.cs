1381fcace6497aeb969e90fa5c3a1d650f06c338
ahuang729 <theantthatdrew@gmail.com>
2023-01-06 13:37:20 -0800

    Movement
    Added basic left/right movement, jumpforce

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb; 
+    [SerializeField] private float speed; // [SerializeField lets you edit in Unity]
+    
+    private bool isGrounded;
+    public float jumpForce;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // GetComponent looks for the component in the inspector
+        isGrounded = true;
+        
+    }
+
+    void FixedUpdate() {
+        // Vector2 for 2D movement; Input.GetAxis("Horizontal") gives -1 or 1 
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y); 
+        if (Input.GetAxis("Horizontal") > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else if (Input.GetAxis("Horizontal") < 0) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)) {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true) {
+            rb.velocity = Vector2.up * jumpForce;
+            if (jumpTimeCounter > 0) {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+
+        // if player stops holding down space they aren't jumping anymore
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            isJumping = false;
+        }
+    }
+
+    
+}
