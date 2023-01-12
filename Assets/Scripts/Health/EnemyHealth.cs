684b97d3db3025357e7525accb695cf55f0dcf18
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-01-12 13:56:44 -0800

    parent halth

--- Assets/Scripts/Health/EnemyHealth.cs
+++ Assets/Scripts/Health/EnemyHealth.cs
@@ -2,28 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealth : ParentHealth
 {
-    [SerializeField]private float startingHealth;
-    public float currentHealth {get; private set;}
+    
     public GameObject damageText;
 
-    private void Awake(){
-        currentHealth = startingHealth;
-    }
-
-    public void TakeDamage(float _damage){
-        
-        if (currentHealth > 0){
-            currentHealth = Mathf.Clamp(currentHealth - _damage,0 , startingHealth);
-            if(currentHealth <=0){
-                Destroy(gameObject);
-            }
-        }
-        else{
-            
-        }
-    }
+    
     private void OnCollisionEnter2D(Collision2D other){
 
     }
---
+++ Assets/Scripts/Health/ParentHealth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentHealth : MonoBehaviour
+{
+    public float startingHealth;
+    public float currentHealth {get; private set;}
+
+    private void Awake(){
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage){
+        
+        if (currentHealth > 0){
+            currentHealth = Mathf.Clamp(currentHealth - _damage,0 , startingHealth);
+            if(currentHealth <=0){
+                Destroy(gameObject);
+            }
+        }
+        else{
+            
+        }
+    }
+}
