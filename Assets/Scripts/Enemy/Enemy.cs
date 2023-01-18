8891d843e9b7634b544c044e1b9d812b6325abfb
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-01-18 14:20:08 -0800

    stuff

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -23,7 +23,15 @@ public class Enemy : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        
+        //
+
+        if(distance < 5){
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+        else{
+                transform.rotation = Quaternion.Euler(0f,0f,0f);
+        }
     }
 }
