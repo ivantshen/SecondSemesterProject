8ac8a5a30eb65efcb25262ca83d22b983fedec02
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-01-18 13:52:30 -0800

    2nd enemy starter

--- Assets/Scripts/Enemy/Enemy2.cs
+++ Assets/Scripts/Enemy/Enemy2.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
-   public GameObject player;
+    public GameObject player;
     public float speed;
     public Vector3 moveOffset;
+    public Transform EnemyTwo;
+    public Rigidbody2D rb;
+    public float direction;
 
     private float distance;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +23,15 @@ public class Enemy2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        
     }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            //EnemyTwo.position = new Vector2(-5f, 0);
+            rb.velocity = new Vector2(direction, 0f);
+        }
+    }
+
+   
 }
