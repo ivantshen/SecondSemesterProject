7a26a416df16987b02e48e7a91036223b6c0ade5
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-01-19 13:28:38 -0800

    Update Enemy2.cs

--- Assets/Scripts/Enemy/Enemy2.cs
+++ Assets/Scripts/Enemy/Enemy2.cs
@@ -26,11 +26,12 @@ public class Enemy2 : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other){
+    private void OnTriggerStay2D(Collider2D other){
         if(rb.velocity == new Vector2(0f,0f)){
         if(other.tag == "Player"){
             //EnemyTwo.position = new Vector2(-5f, 0);
             rb.velocity = new Vector2(direction, 0f);
+            
         }
         }
     }
