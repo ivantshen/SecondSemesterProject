f757d69e9c4370c6a665c5a0d2f0733cde520a0b
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-01-20 14:22:28 -0800

    enemy3

--- Assets/Scripts/Enemy/Enemy3.cs
+++ Assets/Scripts/Enemy/Enemy3.cs
@@ -15,12 +15,22 @@ public class Enemy3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         
-        if(Input.GetKeyDown(KeyCode.W)){
-        transform.position=new Vector2(transform.position.x,transform.position.y+distance); 
+        
+        float disX = Random.Range(-10f,10f);
+        float disY = Random.Range(-50f,5f);
+        if(Input.GetKeyDown(KeyCode.U)){
+            transform.position = new Vector2(Mathf.Clamp(transform.position.x + disX, -30, 10),Mathf.Clamp(transform.position.y + disY, 0, 5));
         }
-        else if(Input.GetKeyDown(KeyCode.D)){
-        transform.position=new Vector2(transform.position.x,transform.position.y-distance); 
-        }
+
+
+
+
+
+        //if(Input.GetKeyDown(KeyCode.W)){
+        //transform.position=new Vector2(transform.position.x,transform.position.y+distance); 
+        //}
+        //else if(Input.GetKeyDown(KeyCode.D)){
+        //transform.position=new Vector2(transform.position.x,transform.position.y-distance); 
+        //}
     }
 }
