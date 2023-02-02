a631b58961e0ad53891b9db6795680be372ab69d
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-02-02 13:45:33 -0800

    spike

---
+++ Assets/Scripts/Enemy/Spike.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag == "Player"){
+            if(other.gameObject){
+            other.gameObject.GetComponent<HealthP1>().TakeDamage(5);    
+            }
+             
+        }
+     }
+}
