a75d564cd93dc4181530ab6e4629e6b43b588c9d
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-02-08 13:42:22 -0800

    real iframes

--- Assets/Scripts/Health/HealthP1.cs
+++ Assets/Scripts/Health/HealthP1.cs
@@ -7,35 +7,40 @@ public class HealthP1 : MonoBehaviour
 {
    
      [SerializeField] private float iFrameTime;
+     private bool allowDmg;
 
     public float startingHealth;
     public float currentHealth {get; private set;}
 
     private void Awake(){
+        allowDmg = true;
         currentHealth = startingHealth;
         Physics2D.IgnoreLayerCollision(0,6,false);
     }
 
     public void TakeDamage(float _damage){
-        
+        if(allowDmg){
         if (currentHealth > 0){
             currentHealth = Mathf.Clamp(currentHealth - _damage,0 , startingHealth);
             StartCoroutine(Invulnerability());
             if(currentHealth <=0){
                 Destroy(gameObject);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                Physics2D.IgnoreLayerCollision(0,6,false);
+                //Physics2D.IgnoreLayerCollision(0,6,false);
             }
         }
         else{
             
         }
+        }
     }
 
     private IEnumerator Invulnerability(){
-        Physics2D.IgnoreLayerCollision(0,6,true);
+        //Physics2D.IgnoreLayerCollision(0,6,true);
+        allowDmg = false;
         yield return new WaitForSeconds(iFrameTime);
-        Physics2D.IgnoreLayerCollision(0,6,false);
+        //Physics2D.IgnoreLayerCollision(0,6,false);
+        allowDmg = true;
     }
 
     private void OnCollisionEnter2D(Collision2D other){
