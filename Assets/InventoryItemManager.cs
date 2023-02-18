f19b60cd38b086c399ee99c9fe8db7bffe798cab
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-02-17 19:30:02 -0800

    inventory progress

--- Assets/InventoryItemManager.cs
+++ Assets/InventoryItemManager.cs
@@ -7,8 +7,20 @@ public class InventoryItemManager : MonoBehaviour,IPointerDownHandler,IPointerUp
 {
     private bool mouseDown = false;
     private GameObject[] inventorySlots;
+    private int itemSlotIndex = -1;
+    private InventoryManager inventory;
+
     void Start(){
         inventorySlots = GameObject.FindGameObjectsWithTag("InventorySlot");
+        inventory = GameObject.FindWithTag("InventorySystem").GetComponent<InventoryManager>();
+        GameObject[] storedItems = inventory.getStoredItems();
+        for(int i= 0;i<storedItems.Length;i++){
+            if(storedItems[i]==null){
+                inventory.storeItem(i,gameObject,itemSlotIndex);
+                itemSlotIndex = i;
+                return;
+            }
+        }
     }
     void Update(){
         if(mouseDown){
@@ -22,7 +34,8 @@ public class InventoryItemManager : MonoBehaviour,IPointerDownHandler,IPointerUp
             mouseDown = false;
             Transform closestSlot = getClosestSlot().transform;
             if(Vector3.Distance(closestSlot.position,transform.position)<20){
-                transform.position = closestSlot.position;
+                inventory.storeItem(getClosestSlotIndex(),gameObject,itemSlotIndex);
+                itemSlotIndex = getClosestSlotIndex();
             }
     }
     private GameObject getClosestSlot(){
@@ -33,6 +46,18 @@ public class InventoryItemManager : MonoBehaviour,IPointerDownHandler,IPointerUp
             if(distance<minDistance){
                 minDistance = distance;
                 closest = slot;
+            }
+        }
+        return closest;
+    }
+    private int getClosestSlotIndex(){
+        float minDistance = 20000;
+        int closest = 0;
+        for(int i=0;i<inventorySlots.Length;i++){
+            float distance = Vector3.Distance(inventorySlots[i].transform.position,transform.position);
+            if(distance<minDistance){
+                minDistance = distance;
+                closest = i;
             }
         }
         return closest;
--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    GameObject[] storedItems;
-    void Update()
+    private GameObject[] storedItems;
+    private GameObject[] inventorySlots;
+    void Start()
     {
+        inventorySlots = GameObject.FindGameObjectsWithTag("InventorySlot");
+       storedItems = new GameObject[GameObject.FindGameObjectsWithTag("InventorySlot").Length];
+    }
+    public void storeItem(int index, GameObject item, int previousItemIndex){
+        if(!storedItems[index]){
+            if(previousItemIndex>-1){
+            storedItems[previousItemIndex] = null;    
+            }
+            storedItems[index] = item;
+        }else{
+            GameObject tempStored = storedItems[index];
+            storedItems[index] = item;
+            storedItems[previousItemIndex]  = tempStored;
+            storedItems[previousItemIndex].transform.position = inventorySlots[previousItemIndex].transform.position;
+        }
+        storedItems[index].transform.position = inventorySlots[index].transform.position;
+    }
+    public GameObject[] getStoredItems(){
+        return storedItems;
     }
 }
