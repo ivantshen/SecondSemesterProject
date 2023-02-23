c83c0387fd16f26752efb4175e1710bd5335148b
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-02-23 13:25:07 -0800

    shop

---
+++ Assets/Scripts/UI/buttonInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonInfo : MonoBehaviour
+{
+    public int ItemID;
+    public Text PriceTxt;
+    public Text QuantityTxt;
+    public GameObject ShopManager;
+
+    void Update(){
+
+    }
+}
