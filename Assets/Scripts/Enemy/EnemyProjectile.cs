4b64998364755a8f48d1262c4a05c5ee5590fa82
nathanyu8th <66180870+nathanyu8th@users.noreply.github.com>
2023-03-14 14:22:24 -0700

    ENMEY 3 DONE SUCK IT OVAN

--- Assets/Scripts/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -5,20 +5,60 @@ using UnityEngine;
 public class EnemyProjectile : MonoBehaviour
 {
     public float bulletDeathTime = 5f;
+    [SerializeField] GameObject explosion;
+    private Rigidbody2D rb;
+    public float bulletSpeed = 10f;
+        private GameObject player;
+
+
+    public float speed;
+
+    private float distance;
+
+    
+    void Start(){
+        player = GameObject.FindWithTag("Player");
+
+        rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = transform.right*bulletSpeed;
+    }
 
     void Update()
     {
+
         if(bulletDeathTime>0){
             bulletDeathTime-= Time.deltaTime;
         }else{
             Destroy(gameObject);
         }
+
+        //rb.velocity = transform.right*bulletSpeed;
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        
+        //
+
+        //if(distance < 10){
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        //}
+
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
             other.gameObject.GetComponent<HealthP1>().TakeDamage(20);    
             Destroy(gameObject); 
+            Instantiate(explosion,transform.position,Quaternion.identity,null);
+        }
+        else if(other.gameObject.layer != 6){
+            Destroy(gameObject);
+        }
+        else{
+        Instantiate(explosion,transform.position,Quaternion.identity,null);
         }
         //else if(other.gameObject.tag!="Player"&&!wallClipping&&other.gameObject.layer!=7){
             // Destroy(gameObject);
