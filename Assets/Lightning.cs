517c8d78fb4c9f23663d035d97d76464c717e13e
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-03-14 13:38:33 -0700

    .

--- Assets/Lightning.cs
+++ Assets/Lightning.cs
@@ -29,14 +29,4 @@ public class Lightning : MonoBehaviour
             Destroy(l,0.3f);
         }
     }
-    void OnTriggerExit2D(Collider2D other){
-        if(other.gameObject.layer==6){
-            float angle = Mathf.Atan2(other.transform.position.y-transform.position.y,other.transform.position.x-transform.position.x)*Mathf.Rad2Deg;
-            GameObject l = Instantiate(lightning,transform.position,Quaternion.Euler(0,0,angle));
-            cm.increaseHitcount(1);
-            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-1)*knockdownForce,ForceMode2D.Impulse);
-            other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
-            Destroy(l,0.3f);
-        }
-    }
 }
