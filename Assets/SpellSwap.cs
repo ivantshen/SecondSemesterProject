3190a0858dcb45fff0cf7de3a78855c239697251
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-03-16 14:22:20 -0700

    .

--- Assets/SpellSwap.cs
+++ Assets/SpellSwap.cs
@@ -8,6 +8,8 @@ public class SpellSwap : MonoBehaviour
     [SerializeField] private GameObject[] spells;
     [SerializeField] private float[] spellCooldown;
     [SerializeField] private float[] playerFreezeTime;
+    [SerializeField] private Color[] colors;
+    [SerializeField] private SpriteRenderer colorIndicator;
     private float[] stashedCooldowns;
     private int spellIndex = 0;
     private WeaponFireScript wp;
@@ -19,6 +21,7 @@ public class SpellSwap : MonoBehaviour
         wp.setFireRate(spellCooldown[spellIndex]);
         wp.setFreezeTime(playerFreezeTime[spellIndex]);
         wp.setCooldown(stashedCooldowns[spellIndex]);
+        colorIndicator.color = colors[spellIndex];
     }
     void Update(){
         for(int i=0;i<stashedCooldowns.Length;i++){
@@ -39,6 +42,7 @@ public class SpellSwap : MonoBehaviour
         wp.setFireRate(spellCooldown[spellIndex]);    
         wp.setFreezeTime(playerFreezeTime[spellIndex]);
         wp.setCooldown(stashedCooldowns[spellIndex]);
+        colorIndicator.color = colors[spellIndex];
         }
         
     }
