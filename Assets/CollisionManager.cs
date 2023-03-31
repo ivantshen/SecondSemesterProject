35627ec9cb86c372fd455164cbe27c61ed0f644e
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-03-31 00:30:50 -0700

    .

--- Assets/CollisionManager.cs
+++ Assets/CollisionManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CollisionManager : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -16,6 +16,18 @@ public class CollisionManager : MonoBehaviour
         for(int i=0;i<12;i++){
         Physics2D.IgnoreLayerCollision(11,i,true);    
         }
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Physics2D.IgnoreLayerCollision(3,3,true);
+        Physics2D.IgnoreLayerCollision(3,9,true);
+        Physics2D.IgnoreLayerCollision(3,7,true);
+        for(int i=0;i<13;i++){
+        Physics2D.IgnoreLayerCollision(10,i,true);    
+        }
+        for(int i=0;i<12;i++){
+        Physics2D.IgnoreLayerCollision(11,i,true);    
+        }
     }
 }
