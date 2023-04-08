998f7a6399875c82d476aaad4a11a7b12af99dfe
nathanyu8th <nathanyu8th@gmail.com>
2023-04-08 13:24:18 -0700

    more stuff

--- Assets/Scripts/Enemy/Enemy2.cs
+++ Assets/Scripts/Enemy/Enemy2.cs
@@ -8,11 +8,14 @@ public class Enemy2 : MonoBehaviour
     private Rigidbody2D rb;
     public float force;
     public float timeBetweenDash;
+    private float reactionTime = 1f;
     private bool canDash = true;
     private float dashCooldown =0f;
+    private SpriteRenderer sr;
     void Start(){
         player = PlayerPersistence.Instance;
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
     
 
@@ -23,11 +26,23 @@ public class Enemy2 : MonoBehaviour
         else{
             canDash = true;
         }
+        if(reactionTime > 0){
+            reactionTime -= Time.deltaTime;
+        }
         if(Vector2.Distance(player.transform.position,transform.position)<5&&canDash){
+            
             Vector2 travel = (player.transform.position - transform.position).normalized * force;
             rb.velocity = new Vector2(travel.x,0f);
             canDash = false;
             dashCooldown = timeBetweenDash;
+            //sr.color = new Color(154,0,135,255);
+        }
+        if (rb.velocity.x > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            
+        } else if (rb.velocity.x < 0) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            
         }
     }
 
