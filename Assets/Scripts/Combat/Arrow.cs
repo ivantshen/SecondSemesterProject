d152bdaf7f6acbe614055eccfbe425d1ed4573da
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-04-17 16:37:28 -0700

    .

--- Assets/Scripts/Combat/Arrow.cs
+++ Assets/Scripts/Combat/Arrow.cs
@@ -14,18 +14,25 @@ public class Arrow : MonoBehaviour
     [SerializeField] private float percentDamageLossPerTargetHit;
     [SerializeField] private int numTargets;
     [SerializeField] private float timeTillDeath;
+    [SerializeField] private float minFallSpeed;
+    [SerializeField] private float maxFallSpeed;
+    private Collider2D col;
     private float finalDamage;
     private float finalForce;
     private float finalKnockback;
+    private float finalFallSpeed;
     private bool dying = false;
     private bool awake = false;
     private Rigidbody2D rb;
+    private ComboManager cm;
     private float finalRot;
     private bool allowPosStop = true;
-    
     void Start(){
         rb = GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
+        cm = PlayerPersistence.Instance.GetComponent<ComboManager>();
+        col = GetComponent<Collider2D>();
+        col.enabled = false;
         if(piercing){
             allowPosStop = false;
         }
@@ -39,24 +46,27 @@ public class Arrow : MonoBehaviour
             }
         }
         if(awake){
-        transform.up =Vector3.Slerp(transform.up, rb.velocity.normalized, Time.deltaTime*3);    
+        transform.up =Vector3.Slerp(transform.up, rb.velocity.normalized, Time.deltaTime*finalFallSpeed);    
         }
         
     }
     // Start is called before the first frame update
     public void assignStatsAndFire(float chargeValue,Vector3 dir){
         transform.SetParent(null);
-        finalDamage = chargeValue*(maximumDamage-minimumDamage) + minimumDamage;
+        finalDamage = (chargeValue*(maximumDamage-minimumDamage) + minimumDamage);
         finalForce = chargeValue*(arrowMaximumForce-arrowMinimumForce) + arrowMinimumForce;
         finalKnockback = chargeValue*(maximumKnockback-minimumKnockback) + minimumKnockback;
+        finalFallSpeed = chargeValue*(maxFallSpeed-minFallSpeed) + minFallSpeed;
         rb.isKinematic = false;
+        col.enabled = true;
         awake = true;
         rb.AddForce(finalForce*dir);
     }
     void OnTriggerEnter2D(Collider2D other){
         if(awake){
         if(other.gameObject.layer==6){
-            other.GetComponent<EnemyHealth>().TakeDamage(finalDamage);
+            other.GetComponent<EnemyHealth>().TakeDamage(finalDamage*cm.getComboDamageMultiplier());
+            cm.increaseHitcount(1);
             if(allowPosStop){
                 dying = true;    
                 awake = false;
