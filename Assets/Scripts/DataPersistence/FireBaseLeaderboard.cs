7977fa526be52d29fd675ae1f7861f95c1c6e144
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-04-21 13:30:58 -0700

    .

---
+++ Assets/Scripts/DataPersistence/FireBaseLeaderboard.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+
+public class FireBaseLeaderboard : MonoBehaviour
+{
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+    public DatabaseReference DBreference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
