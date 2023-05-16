9d53ad706c839e2cb7608f316ffa861367af8250
ivantshen <116920615+ivantshen@users.noreply.github.com>
2023-05-16 14:22:26 -0700

    push

--- Assets/FloorCrumble.cs
+++ Assets/FloorCrumble.cs
@@ -6,18 +6,39 @@ public class FloorCrumble : MonoBehaviour
 {
     public GameObject floor;
     [SerializeField] private AudioSource breakSoundEffect;
-
+    private float cd;
+    private SpriteRenderer sr;
+    private Collider2D col;
+    [SerializeField] private bool respawn = true;
+    void Start(){
+        col = gameObject.GetComponent<Collider2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Player") {
+        if (collision.gameObject.tag == "Player"&&collision.gameObject.transform.position.y>transform.position.y+0.75f) {
             StartCoroutine(crumbleMyFloor());
         }
     }
-
+    private void Update(){
+        if(cd>0){
+            cd-= Time.deltaTime;
+        }else{
+            sr.enabled = true;
+            col.enabled = true;
+        }
+    }
     IEnumerator crumbleMyFloor() {
         yield return new WaitForSeconds(0.2f);
         breakSoundEffect.Play();
         yield return new WaitForSeconds(0.3f);
-        Destroy(floor);
+        if(respawn){
+        col.enabled = false;
+        sr.enabled = false;
+        cd = 5;    
+        }else{
+        Destroy(gameObject);
+        }
+        
     }
     
 }
